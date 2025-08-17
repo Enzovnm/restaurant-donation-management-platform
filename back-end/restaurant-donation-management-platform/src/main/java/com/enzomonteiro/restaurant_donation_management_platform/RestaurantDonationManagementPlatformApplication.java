bfe2d8b39ff806beddf2ee5762006004397c530e
Enzo Monteiro <enzovila.monteiro@gmail.com>
2025-08-17 17:41:29 -0300

    chore: initializing Spring Boot

---
+++ back-end/restaurant-donation-management-platform/src/main/java/com/enzomonteiro/restaurant_donation_management_platform/RestaurantDonationManagementPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.enzomonteiro.restaurant_donation_management_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantDonationManagementPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantDonationManagementPlatformApplication.class, args);
+	}
+
+}
---
+++ back-end/restaurant-donation-management-platform/src/test/java/com/enzomonteiro/restaurant_donation_management_platform/RestaurantDonationManagementPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enzomonteiro.restaurant_donation_management_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantDonationManagementPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
