fd8785e51427c622f0963db9cfa4f95f2f992300
Enzo Monteiro <enzovila.monteiro@gmail.com>
2025-08-24 20:05:41 -0300

    feat: Adding findByEmail

---
+++ back-end/restaurant-donation-management-platform/src/main/java/com/enzomonteiro/restaurant_donation_management_platform/repositories/RestaurantRepository.java
@@ -0,0 +1,14 @@
+package com.enzomonteiro.restaurant_donation_management_platform.repositories;
+
+import com.enzomonteiro.restaurant_donation_management_platform.entities.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long > {
+
+
+    Optional<Restaurant> findByEmail(String email);
+}
