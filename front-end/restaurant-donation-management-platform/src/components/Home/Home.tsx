7c6dc7b31fd14da09197f5c834c6d3485fee2703
Enzo Monteiro <enzovila.monteiro@gmail.com>
2025-08-30 21:42:54 -0300

    feat: HomePage completed

--- front-end/restaurant-donation-management-platform/src/components/Home/Home.tsx
+++ front-end/restaurant-donation-management-platform/src/components/Home/Home.tsx
@@ -1,9 +1,8 @@
-import React from "react";
 import imgDonate from "../../assets/donate-image.png";
 
 export const Home = () => {
   return (
-    <main className="flex w-screen h-screen bg-[#f6f6f6]">
+    <main className="flex w-screen h-hero bg-[#f6f6f6]">
       <div className="w-full h-full flex items-center justify-between">
         <div className="flex-2 text-center">
           <h1 className="text-3xl uppercase font-bold">
@@ -13,6 +12,9 @@ export const Home = () => {
             Uma chance para todos aqueles que lutam contra a fome
           </h2>
           <h3 className="mt-4">Doe uma marmita. Seja solidário!</h3>
+          <button className="bg-[#e94911] text-white  px-8 py-4 rounded-full mt-4 cursor-pointer">
+            FAÇA UMA DOAÇÃO
+          </button>
         </div>
         <div className="flex-2">
           <img className="w-11/12" src={imgDonate} />
--- front-end/restaurant-donation-management-platform/src/components/Navbar/Navbar.tsx
+++ front-end/restaurant-donation-management-platform/src/components/Navbar/Navbar.tsx
@@ -1,5 +1,20 @@
 import React from "react";
+import imgLogo from "../../assets/logo.png";
 
 export const Navbar = () => {
-  return <nav></nav>;
+  return (
+    <nav className="w-full px-8 py-4  flex items-center justify-between">
+      <div>
+        <img src={imgLogo} />
+      </div>
+      <ul>
+        <li>
+          <a href="">Últimas doações</a>
+        </li>
+        {/* <li>
+          <a href="">Admin</a>
+        </li> */}
+      </ul>
+    </nav>
+  );
 };
